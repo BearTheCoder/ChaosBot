7c762fca2320dc9e2301e125771581eefcf0992b
AaronAdkins <58434823+AaronAdkins@users.noreply.github.com>
2022-09-28 16:24:55 -0500

    test subscriberbot integration 1

--- SubscriberBot/SubscriberBot.js
+++ SubscriberBot/SubscriberBot.js
@@ -1,5 +1,84 @@
-function TestFunc() {
-    console.log(`SubscriberBot...`);
+/*
+  Current issues:
+    1.) If a role is changed in Discord (A role not controlled by the bot), the event "guildMemberUpdate"
+    fires as designed. If the bot THEN makes a change, the event fires a second time needlessly.
+    This should be controlled for optimization.
+    2.) The bot currently does not have the access to send messages in chat (I think).
+    I need to test whether or not I need to change access levels in order to send messages into channels.
+    If I need to update permissions, do I need to remove the current bot and add a new bot in?
+*/
+
+// Variable Init -------------------------------------------------------------------------------------
+
+const AWS = require("aws-sdk"); // Needed for hidden variables using Heroku
+
+const S3 = new AWS.S3({
+    MyGuildID: process.env.GuildID,
+    BearUserID: process.env.BearID,
+});
+
+const {
+    ButtonBuilder,
+    ButtonStyle,
+    ActionRowBuilder,
+} = require("discord.js");
+
+const BTN = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+        .setCustomId("roleupdate")
+        .setLabel("Reload user roles?")
+        .setStyle(ButtonStyle.Primary)
+);
+
+async function SendButton(message) {
+    await message.author.send({
+        content: `If you believe there to be an issue with ChaosBot please contact BearTheCoder#6820 for debugging purposes.
+            The button below will iterate through all users in the server and update the roles accordingly. Please be advised, if there are a
+            large number of members this may take a while...`,
+        components: [ BTN ],
+    });
 }
 
-module.exports = { TestFunc };
+function UpdateUserRoles(CurrentMember) {
+    console.log(`Updating roles for user ${CurrentMember.displayName}...`);
+    let hasKittenRole = false;
+    let hasPhweakRole = false;
+    let hasPhweettenRole = false;
+    CurrentMember.roles.cache.forEach((UserRole) => {
+        if (UserRole.name.toLowerCase().includes("kittens")) {
+            hasKittenRole = true;
+        } else if (UserRole.name.toLowerCase().includes("phweaks")) {
+            hasPhweakRole = true;
+        } else if (UserRole.name.toLowerCase().includes("phweettens")) {
+            hasPhweettenRole = true;
+        }
+    });
+    //The following lines run ASYNC and takes a while for the results to show on DISCORD...
+    let CombinedRole = Individual.roles.cache.find((FindCombinedRole) =>
+        FindCombinedRole.name.toLowerCase().includes("phweettens")
+    );
+    if (hasKittenRole && hasPhweakRole && !hasPhweettenRole) {
+        Individual.roles.add(CombinedRole);
+    } else if (!hasKittenRole || !hasPhweakRole) {
+        Individual.roles.remove(CombinedRole);
+    }
+}
+
+function UpdateAllRoles() {
+    console.log(`Roles updated...`);
+    const Guild = client.guilds.cache.get(S3.config.MyGuildID);
+    Guild.members.fetch().then((ListOfMembers) => {
+        ListOfMembers.forEach((Individual) => {
+            UpdateUserRoles(Individual);
+        });
+    });
+}
+
+async function SendMessage(LocalError) {
+    const BTC = await client.users.fetch(S3.config.BearUserID);
+    BTC.send(`The bot has experienced and error: ${LocalError} \n 
+    Please go to https://www.heroku.com to check error logs.
+  `);
+}
+
+module.exports = { SendButton, UpdateUserRoles, UpdateAllRoles, SendMessage };
--- main.js
+++ main.js
@@ -5,16 +5,30 @@
         Only issue, I have no clue how to create actual slash functions yet.
 */
 
+// Access other scripts in directory
 const LarryBot = require(`./LarryBot/LarryBot.js`);
 const SB_Bot = require(`./SubscriberBot/SubscriberBot.js`);
+
+//Global vars
 const AWS = require(`aws-sdk`); // Needed for hidden variables using Heroku
-const S3 = new AWS.S3({ MyToken: process.env.Token });
-const { Client, GatewayIntentBits } = require(`discord.js`);
+
+const S3 = new AWS.S3({
+    MyToken: process.env.Token,
+    MyGuildID: process.env.GuildID,
+    MyUserID: process.env.UserID,
+});
+
+const {
+    Client,
+    GatewayIntentBits,
+} = require("discord.js");
+
 const MyClient = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
     ],
 });
 
@@ -22,8 +36,31 @@ console.log(`Main.js Loaded...`);
 
 // Event Listener - Needs to be changed to a slash function.
 MyClient.on(`messageCreate`, async (message) => {
-    LarryBot.LarryFunc(message);
-    SB_Bot.TestFunc();
+    if (message.content.toLowerCase().includes("//roleupdate")) {
+        SB_Bot.SendButton(message);
+    }
+    else if (message.content.toLowerCase().includes("//larry")) {
+        LarryBot.LarryFunc(message);
+    }
+});
+
+client.on("guildMemberUpdate", (newMember) => {
+    try {
+        SB_Bot.UpdateUserRoles(newMember);
+    } catch (ErrorMsg) {
+        SB_Bot.SendMessage(ErrorMsg);
+    }
+});
+
+MyClient.on("interactionCreate", async (iAction) => {
+    if (!iAction.customId === "roleupdate") return;
+    //Put Action Here -------------------------------
+    SB_Bot.UpdateAllRoles();
+    await iAction.reply({
+        content: "All roles are being updated... The orignal message has been deleted to prevent multiple tasks.",
+        ephemeral: true, //This makes the message delete after a certain period of time
+    });
+    iAction.message.delete();
 });
 
 MyClient.login(S3.config.MyToken);

eb74ae3d9dff6a02381fff8fe02c54f52e11626a
BearTheCoder <bearthecoder@gmail.com>
2022-10-02 19:34:53 -0400

    Added functions to test for JSON parsing

--- main.js
+++ main.js
@@ -10,60 +10,69 @@ const LarryBot = require(`./LarryBot/LarryBot.js`);
 const SB_Bot = require(`./SubscriberBot/SubscriberBot.js`);
 const Magic8Ball = require(`./Magic8Ball/Magic8Ball.js`);
 
+//Delete all below after testing - START
+const TestJson = require(`./TestFolder/TestJson.js`);
+const FileName_Node = "FILENAME.json";
+const FileName_Script = "node_modules/FILENAME.json";
+let JSONObject = require(FileName_Node); //node function
+const FS = require("fs");
+//END
+
 //Global vars
 const AWS = require(`aws-sdk`); // Needed for hidden variables using Heroku
 const S3 = new AWS.S3({
-    MyToken: process.env.Token,
-    MyGuildID: process.env.GuildID,
-    MyUserID: process.env.BearID,
+  MyToken: process.env.Token,
+  MyGuildID: process.env.GuildID,
+  MyUserID: process.env.BearID,
 });
 
-const {
-    Client,
-    GatewayIntentBits,
-} = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const MyClient = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
 });
 
 console.log(`Main.js Loaded...`);
 
 // Needs to be changed to a slash function.
 MyClient.on(`messageCreate`, async (message) => {
-    if (message.content.toLowerCase().includes("//roleupdate")) {
-        SB_Bot.SendButton(message);
-    }
-    else if (message.content.toLowerCase().includes("//larry")) {
-        LarryBot.LarryFunc(message);
-    }
-    else if (message.content.toLowerCase().includes("//8ball")) {
-        Magic8Ball.Magic8Ball(message);
-    }
+  if (message.content.toLowerCase().includes("//roleupdate")) {
+    SB_Bot.SendButton(message);
+  } else if (message.content.toLowerCase().includes("//larry")) {
+    LarryBot.LarryFunc(message);
+  } else if (message.content.toLowerCase().includes("//8ball")) {
+    Magic8Ball.Magic8Ball(message);
+  }
+  //This else if is used for testing and can be deleted later - START
+  else if (message.content.toLowerCase().includes("//testJSON")) {
+    TestJson.TestJson(message, JSONObject, FileName_Script);
+  }
+  //END
 });
 
 MyClient.on("guildMemberUpdate", (newMember) => {
-    try {
-        SB_Bot.UpdateUserRoles(newMember);
-    } catch (ErrorMsg) {
-        SB_Bot.SendMessage(ErrorMsg);
-    }
+  try {
+    SB_Bot.UpdateUserRoles(newMember);
+  } catch (ErrorMsg) {
+    SB_Bot.SendMessage(ErrorMsg);
+  }
 });
 
 MyClient.on("interactionCreate", async (iAction) => {
-    if (!iAction.customId === "roleupdate") return;
-    //Put Action Here -------------------------------
-    SB_Bot.UpdateAllRoles(MyClient);
-    await iAction.reply({
-        content: "All roles are being updated... The orignal message has been deleted to prevent multiple tasks.",
-        ephemeral: true, //This makes the message delete after a certain period of time
-    });
-    iAction.message.delete();
+  if (!iAction.customId === "roleupdate") return;
+  //Put Action Here -------------------------------
+  SB_Bot.UpdateAllRoles(MyClient);
+  await iAction.reply({
+    content:
+      "All roles are being updated... The orignal message has been deleted to prevent multiple tasks.",
+    ephemeral: true, //This makes the message delete after a certain period of time
+  });
+  iAction.message.delete();
 });
 
 MyClient.login(S3.config.MyToken);

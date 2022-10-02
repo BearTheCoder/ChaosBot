f7c6d4f965611ce6a121c6d5e5466faa65846414
BearTheCoder <bearthecoder@gmail.com>
2022-10-02 19:51:23 -0400

    Removed testing file and editted subscriber bot

--- SubscriberBot/SubscriberBot.js
+++ SubscriberBot/SubscriberBot.js
@@ -13,70 +13,66 @@
 const AWS = require("aws-sdk"); // Needed for hidden variables using Heroku
 
 const S3 = new AWS.S3({
-    MyGuildID: process.env.GuildID,
-    BearUserID: process.env.BearID,
+  MyGuildID: process.env.GuildID,
+  BearUserID: process.env.BearID,
 });
 
-const {
-    ButtonBuilder,
-    ButtonStyle,
-    ActionRowBuilder,
-} = require("discord.js");
+const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
 
 const BTN = new ActionRowBuilder().addComponents(
-    new ButtonBuilder()
-        .setCustomId("roleupdate")
-        .setLabel("Reload user roles?")
-        .setStyle(ButtonStyle.Primary)
+  new ButtonBuilder()
+    .setCustomId("roleupdate")
+    .setLabel("Reload user roles?")
+    .setStyle(ButtonStyle.Primary)
 );
 
 async function SendButton(message) {
-    await message.author.send({
-        content: `If you believe there to be an issue with ChaosBot please contact BearTheCoder#6820 for debugging purposes.
-            The button below will iterate through all users in the server and update the roles accordingly. Please be advised, if there are a
-            large number of members this may take a while...`,
-        components: [ BTN ],
-    });
+  await message.author.send({
+    content: `If you believe there to be an issue with ChaosBot please contact BearTheCoder#6820 for debugging purposes. The button below will iterate through all users in the server and update the roles accordingly. Please be advised, if there are a large number of members this may take a while...`,
+    components: [BTN],
+  });
 }
 
 function UpdateUserRoles(CurrentMember) {
-    console.log(`Updating roles for user ${CurrentMember.displayName}...`);
-    let hasKittenRole = false;
-    let hasPhweakRole = false;
-    let hasPhweettenRole = false;
-    CurrentMember.roles.cache.forEach((UserRole) => {
-        if (UserRole.name.toLowerCase().includes("kittens")) {
-            hasKittenRole = true;
-        } else if (UserRole.name.toLowerCase().includes("phweaks")) {
-            hasPhweakRole = true;
-        } else if (UserRole.name.toLowerCase().includes("phweettens")) {
-            hasPhweettenRole = true;
-        }
-    });
-    //The following lines run ASYNC and takes a while for the results to show on DISCORD...
-    let CombinedRole = CurrentMember.roles.cache.find((FindCombinedRole) =>
-        FindCombinedRole.name.toLowerCase().includes("phweettens")
-    );
-    if (hasKittenRole && hasPhweakRole && !hasPhweettenRole) {
-        CurrentMember.roles.add(CombinedRole);
-    } else if ((!hasKittenRole || !hasPhweakRole) && hasPhweettenRole) {
-        CurrentMember.roles.remove(CombinedRole);
+  console.log(`Updating roles for user ${CurrentMember.displayName}...`);
+  let hasKittenRole = false;
+  let hasPhweakRole = false;
+  let hasPhweettenRole = false;
+  CurrentMember.roles.cache.forEach((UserRole) => {
+    if (UserRole.name.toLowerCase().includes("kittens")) {
+      hasKittenRole = true;
+    } else if (UserRole.name.toLowerCase().includes("phweaks")) {
+      hasPhweakRole = true;
+    } else if (UserRole.name.toLowerCase().includes("phweettens")) {
+      hasPhweettenRole = true;
     }
+  });
+  //The following lines run ASYNC and takes a while for the results to show on DISCORD...
+  let CombinedRole = CurrentMember.roles.cache.find((FindCombinedRole) =>
+    FindCombinedRole.name.toLowerCase().includes("phweettens")
+  );
+  if (hasKittenRole && hasPhweakRole && !hasPhweettenRole) {
+    CurrentMember.roles.add(CombinedRole);
+    console.log(`Added role to ${CurrentMember}`);
+  } else if ((!hasKittenRole || !hasPhweakRole) && hasPhweettenRole) {
+    CurrentMember.roles.remove(CombinedRole);
+    console.log(`Removed role to ${CurrentMember}`);
+  }
 }
 
 function UpdateAllRoles(client) {
-    console.log(`Roles updated...`);
-    const Guild = client.guilds.cache.get(S3.config.MyGuildID);
-    Guild.members.fetch().then((ListOfMembers) => {
-        ListOfMembers.forEach((CurrentMember) => {
-            UpdateUserRoles(CurrentMember);
-        });
+  console.log(`Roles updated...`);
+  const Guild = client.guilds.cache.get(S3.config.MyGuildID);
+  Guild.members.fetch().then((ListOfMembers) => {
+    ListOfMembers.forEach((CurrentMember) => {
+      UpdateUserRoles(CurrentMember);
     });
+  });
 }
 
 async function SendMessage(LocalError) {
-    const BTC = await client.users.fetch(S3.config.BearUserID);
-    BTC.send(`The bot has experienced and error: ${LocalError} \n 
+  const BTC = await client.users.fetch(S3.config.BearUserID);
+  BTC.send(`The bot has experienced and error: ${LocalError} \n 
     Please go to https://www.heroku.com to check error logs.
   `);
 }

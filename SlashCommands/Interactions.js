1e2463f440d89f21f22084a103fca39fae89552b
BearTheCoder <bearthecoder@gmail.com>
2022-10-23 14:46:39 -0400

    added test command interaction

--- SlashCommands/Interactions.js
+++ SlashCommands/Interactions.js
@@ -40,6 +40,12 @@ interactions = [
       await interaction.reply(coinFlipResult);
     },
   },
+  {
+    commandName: `testcommand`,
+    commandFunction: async function (interaction, myClient) {
+      await interaction.reply("Test");
+    },
+  },
 ];
 
 module.exports = { interactions };

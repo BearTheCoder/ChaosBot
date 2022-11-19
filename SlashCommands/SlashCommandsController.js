7aaec972087c8bfed3e5935e49e8dbe3969a9163
BearTheCoder <bearthecoder@gmail.com>
2022-11-19 18:14:24 -0500

    debug

--- SlashCommands/SlashCommandsController.js
+++ SlashCommands/SlashCommandsController.js
@@ -196,8 +196,12 @@ function startRubberLarry(interaction, myClient) {
       typingTimeout = setTimeout(() => {
         if (canReply) {
           console.log(rubberLarryPhrases[0]);
-          const randNum = Math.floor(Math.random * rubberLarryPhrases.length);
+          const randNum = Math.random;
           console.log(randNum);
+          const randArray = randNum * rubberLarryPhrases.length;
+          console.log(randArray);
+          const flooredRand = Math.floor(randArray);
+          console.log(flooredRand);
           typing.channel.send(rubberLarryPhrases[randNum]);
           canReply = false;
         }

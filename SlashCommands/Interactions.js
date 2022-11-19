1eadf95f045f153036a4bbd1241103755bdd81c9
BearTheCoder <bearthecoder@gmail.com>
2022-11-19 10:19:58 -0500

    feat: /rubberlarry starting
    Checking to see if "interaction.reply" closes the command chain.

--- SlashCommands/Interactions.js
+++ SlashCommands/Interactions.js
@@ -6,7 +6,7 @@ interactions = [
     commandName: `createcommand`,
     commandFunction: async function (interaction, myClient) {
       await interaction.showModal(slashCommandsController.returnCreateCommandModal()); //sent to "interactionCreate" event listener on main
-      console.log(`${interaction.user.username} has used ${this.commandName}...`)
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
     },
   },
   {
@@ -14,7 +14,7 @@ interactions = [
     commandFunction: async function (interaction, myClient) {
       subscriberBot.updateAllRoles(myClient, process.env.myGuildID);
       await interaction.reply("All roles are being updated...");
-      console.log(`${interaction.user.username} has used ${this.commandName}...`)
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
     },
   },
   {
@@ -22,7 +22,7 @@ interactions = [
     commandFunction: async function (interaction, myClient) {
       slashCommandsController.deleteCommandByID(interaction);
       await interaction.reply("Command deleted...");
-      console.log(`${interaction.user.username} has used ${this.commandName}...`)
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
     },
   },
   {
@@ -30,57 +30,65 @@ interactions = [
     commandFunction: async function (interaction, myClient) {
       slashCommandsController.listCommands(interaction);
       //interaction.reply() is managed by listCommands()
-      console.log(`${interaction.user.username} has used ${this.commandName}...`)
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
     },
   },
   {
     commandName: `coinflip`,
     commandFunction: async function (interaction, myClient) {
       let coinFlipResult = slashCommandsController.returnCoinFlipResult(interaction, myClient);
-      interaction.options.getString('options') === null ? 
-        await interaction.reply(`Result: ${coinFlipResult}`) :
+      interaction.options.getString('options') === null ?
+        await interaction.reply(`Result: ${ coinFlipResult }`) :
         await interaction.reply(`
-          ${interaction.options.getString('options')} \n
-          Result: ${coinFlipResult}
+          ${ interaction.options.getString('options') } \n
+          Result: ${ coinFlipResult }
         `);
-      console.log(`${interaction.user.username} has used ${this.commandName}...`)
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
     },
   },
   {
     commandName: `8ball`,
     commandFunction: async function (interaction, myClient) {
       let reply = slashCommandsController.shake8Ball(interaction);
-      interaction.options.getString('question') === null ? 
-        await interaction.reply(`**Magic 8 Ball Says:** ${reply}`) :
+      interaction.options.getString('question') === null ?
+        await interaction.reply(`**Magic 8 Ball Says:** ${ reply }`) :
         await interaction.reply(`
-          ${interaction.options.getString('question')} \n
-          **Magic 8 Ball Says:** ${reply}
+          ${ interaction.options.getString('question') } \n
+          **Magic 8 Ball Says:** ${ reply }
         `);
-      console.log(`${interaction.user.username} has used ${this.commandName}...`)
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
     },
   },
   {
     commandName: `resetcommands`,
     commandFunction: async function (interaction, myClient) {
-      slashCommandsController.resetCommands(interaction)
+      slashCommandsController.resetCommands(interaction);
       // interaction.reply() is managed by resetCommands()
-      console.log(`${interaction.user.username} has used ${this.commandName}...`)
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
     },
   },
   {
     commandName: `whoislarry`,
     commandFunction: async function (interaction, myClient) {
-      let reply = slashCommandsController.sendLarryInfo()
+      let reply = slashCommandsController.sendLarryInfo();
       await interaction.reply(reply);
-      console.log(`${interaction.user.username} has used ${this.commandName}...`);
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
     },
   },
   {
     commandName: `christmas`,
     commandFunction: async function (interaction, myClient) {
-      let reply = slashCommandsController.timeUntilChristmas()
+      let reply = slashCommandsController.timeUntilChristmas();
       await interaction.reply(reply);
-      console.log(`${interaction.user.username} has used ${this.commandName}...`);
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
+    },
+  },
+  {
+    commandName: `rubberlarry`,
+    commandFunction: async function (interaction, myClient) {
+      await interaction.reply("I'm listening...");
+      await interaction.reply("I'm listening again...");
+      console.log(`${ interaction.user.username } has used ${ this.commandName }...`);
     },
   },
 ];

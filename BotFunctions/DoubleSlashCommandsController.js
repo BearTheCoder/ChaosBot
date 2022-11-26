31b934e682bcf59805f670e8c2373a6eef91de55
BearTheCoder <bearthecoder@gmail.com>
2022-11-26 11:13:00 -0500

    Command controller

--- BotFunctions/DoubleSlashCommandsController.js
+++ BotFunctions/DoubleSlashCommandsController.js
@@ -1,4 +1,5 @@
-const { sendLarryWisdom, logCommands } = require(`./LarryBot/LarryBot.js`);
+const { sendLarryWisdom } = require(`./LarryBot/LarryBot.js`);
+const { logCommands } = require(`./CommandController/CommandController`);
 
 module.exports.commands = [
   {

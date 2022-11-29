8021a75c12793986385b3969cf5f8bae9f6d423d
BearTheCoder <bearthecoder@gmail.com>
2022-11-29 10:07:24 -0500

    fixed roles

--- BotFunctions/SubscriberBot/SubscriberBot.js
+++ BotFunctions/SubscriberBot/SubscriberBot.js
@@ -1,17 +1,20 @@
 // *****     Exports     *****
-function updateUserRoles (currentMember, myPersonalID) {
-  const kittenRole = currentMember.roles.cache.find((role) => role.id === '917969234334744636'); //Kittens
-  const phweeRole = currentMember.roles.cache.find((role) => role.id === '920174030458351646'); //Phweebs
-  const combinedRole = currentMember.roles.cache.find((role) => role.id === '1020691003084128286'); //Phweettens
+function updateUserRoles (currentMember) {
+  const kittenRole = currentMember.roles.cache.find((role) => role.id === '917969234334744636'); //Kittens 917969234334744636
+  const phweeRole = currentMember.roles.cache.find((role) => role.id === '920174030458351646'); //Phweebs 920174030458351646
+  const combinedRole = currentMember.roles.cache.find((role) => role.id === '1020691003084128286'); //Phweettens 1020691003084128286
+  const guildCombinedRole = currentMember.guild.roles.cache.find((role) => role.id === '1020691003084128286'); //Phweettens 1020691003084128286
   if (kittenRole !== undefined && phweeRole !== undefined && combinedRole === undefined) {
-    currentMember.roles.add(combinedRole);
-    console.log(`Added role to ${currentMember.displayName}`);
+    currentMember.roles.add(guildCombinedRole).then(() => {
+      console.log(`Added role to ${currentMember.displayName}`);
+    });
   }
   else if ((kittenRole === undefined || phweeRole === undefined) && combinedRole !== undefined) {
-    currentMember.roles.remove(combinedRole);
-    console.log(`Removed role from ${currentMember.displayName}`);
-  }
-};
+    currentMember.roles.remove(guildCombinedRole).then(() => {
+      console.log(`Removed role from ${currentMember.displayName}`);
+    });
+  };
+}
 
 module.exports.updateAllRoles = (myClient, myGuildID) => {
   const myGuild = myClient.guilds.cache.get(myGuildID);

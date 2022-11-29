a10cca5b5a30e654d2252b03eaf4e425e1739391
BearTheCoder <bearthecoder@gmail.com>
2022-11-28 21:20:44 -0500

    Checking Roles

--- BotFunctions/SubscriberBot/SubscriberBot.js
+++ BotFunctions/SubscriberBot/SubscriberBot.js
@@ -1,32 +1,37 @@
 // *****     Exports     *****
-function updateUserRoles(currentMember, myPersonalID) {
-  try {
-    let hasKittenRole = false;
-    let hasPhweakRole = false;
-    let hasPhweettenRole = false;
-    currentMember.roles.cache.forEach((userRole) => {
-      if (userRole.name.toLowerCase().includes("kittens")) {
-        hasKittenRole = true;
-      } else if (userRole.name.toLowerCase().includes("phweebs")) {
-        hasPhweakRole = true;
-      } else if (userRole.name.toLowerCase().includes("phweettens")) {
-        hasPhweettenRole = true;
-      }
-    });
-    let combinedRole = currentMember.guild.roles.cache.find((findCombinedRoleAsync) =>
-      findCombinedRoleAsync.name.toLowerCase().includes("phweettens")
-    );
-    if (hasKittenRole && hasPhweakRole && !hasPhweettenRole) {
-      currentMember.roles.add(combinedRole);
-      console.log(`Added role to ${currentMember.displayName}`);
-    } else if ((!hasKittenRole || !hasPhweakRole) && hasPhweettenRole) {
-      currentMember.roles.remove(combinedRole);
-      console.log(`Removed role from ${currentMember.displayName}`);
-    }
-  }
-  catch (localError) {
-    sendErrorPM(localError, myPersonalID);
-  }
+function updateUserRoles (currentMember, myPersonalID) {
+
+  console.log(currentMember);
+  console.log(currentMember.roles);
+  console.log(currentMember.roles.cache);
+
+  // try {
+  //   let hasKittenRole = false;
+  //   let hasPhweakRole = false;
+  //   let hasPhweettenRole = false;
+  //   currentMember.roles.cache.forEach((userRole) => {
+  //     if (userRole.name.toLowerCase().includes("kittens")) {
+  //       hasKittenRole = true;
+  //     } else if (userRole.name.toLowerCase().includes("phweebs")) {
+  //       hasPhweakRole = true;
+  //     } else if (userRole.name.toLowerCase().includes("phweettens")) {
+  //       hasPhweettenRole = true;
+  //     }
+  //   });
+  //   let combinedRole = currentMember.guild.roles.cache.find((findCombinedRoleAsync) =>
+  //     findCombinedRoleAsync.name.toLowerCase().includes("phweettens")
+  //   );
+  //   if (hasKittenRole && hasPhweakRole && !hasPhweettenRole) {
+  //     currentMember.roles.add(combinedRole);
+  //     console.log(`Added role to ${currentMember.displayName}`);
+  //   } else if ((!hasKittenRole || !hasPhweakRole) && hasPhweettenRole) {
+  //     currentMember.roles.remove(combinedRole);
+  //     console.log(`Removed role from ${currentMember.displayName}`);
+  //   }
+  // }
+  // catch (localError) {
+  //   sendErrorPM(localError, myPersonalID);
+  // }
 };
 
 module.exports.updateAllRoles = (myClient, myGuildID) => {
@@ -42,7 +47,7 @@ module.exports.updateAllRoles = (myClient, myGuildID) => {
 module.exports.updateUserRoles = updateUserRoles;
 
 // *****     Internal Functions     *****
-async function sendErrorPM(localError, myUserID) {
+async function sendErrorPM (localError, myUserID) {
   const myUserInfo = await client.users.fetch(myUserID);
   myUserInfo.send(`The bot has experienced and error: ${localError} \n 
     Please go to https://raliway.app to check error logs.

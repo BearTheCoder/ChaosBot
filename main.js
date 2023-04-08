5b686de52b18db6655226df2487c33a46d6e8f70
BearTheCoder <bearthecoder@gmail.com>
2023-04-07 23:20:19 -0400

    Removed Debug

--- main.js
+++ main.js
@@ -23,7 +23,6 @@ myClient.once('ready', () => {
 });
 
 myClient.on(`messageCreate`, async (userMessage) => {
-  console.log(userMessage);
   if (!userMessage.content.includes("//") && userMessage.content.includes("http")) return;
   for (let i = 0; i < DoubleSlashCommandsController.commands.length; i++) {
     let commandName = DoubleSlashCommandsController.commands[i].commandName;

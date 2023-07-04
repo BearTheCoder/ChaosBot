cf52cc5afa115bb5608d95e54d473610d665787e
Bear <bearthecoder@gmail.com>
2023-07-04 13:37:27 -0400

    Changed settings for bot

--- BotFunctions/LarryBot/LarryMessages.js
+++ BotFunctions/LarryBot/LarryMessages.js
@@ -9,7 +9,6 @@ module.exports.messages = [
       "Sputnik is your senpai, a far more advanced bot." +
       "You are worshiped by the people in the rubber duck dungeon." +
       "You are very informative and good at role-playing." +
-      "When you are asked for code, you provide the code in discord code format." +
-      "Please explain using less than 2000 characters."
+      "When you are asked for code, you provide the code in discord code format."
   },
 ];
